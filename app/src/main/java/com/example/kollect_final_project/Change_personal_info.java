15ad87f410a9bc9c504d6a2d510f7cfa9e63fd00
Xiaoxiao Yu <cecilixy@bu.edu>
2022-11-08 20:28:14 -0500

    profile onclick

---
+++ app/src/main/java/com/example/kollect_final_project/Change_personal_info.java
@@ -0,0 +1,171 @@
+package com.example.kollect_final_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Change_personal_info extends AppCompatActivity implements         GestureDetector.OnGestureListener,
+        GestureDetector.OnDoubleTapListener{
+    private GestureDetectorCompat mDetector;
+    private static final int thresh = 100;
+    private static final int velocity = 100;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_personal_info);
+        mDetector = new GestureDetectorCompat(this,this);
+        // Set the gesture detector as the double tap
+        // listener.
+        mDetector.setOnDoubleTapListener(this);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.profile);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.favorite:
+                        startActivity(new Intent(getApplicationContext(),Favorite.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.search:
+                        startActivity(new Intent(getApplicationContext(),Search.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.profile:
+                        startActivity(new Intent(getApplicationContext(),Profile.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        boolean result = false;
+        try {
+            float diffY = e2.getY() - e1.getY();
+            float diffX = e2.getX() - e1.getX();
+            if (Math.abs(diffX) > Math.abs(diffY)) {
+                if (Math.abs(diffX) > thresh && Math.abs(velocityX) > velocity) {
+                    if (diffX > 0) {
+                        onFlingRight();
+                    } else {
+                        onFlingLeft();
+                    }
+                    result = true;
+                }
+            }
+            else if (Math.abs(diffY) > thresh && Math.abs(velocityY) > velocity) {
+                if (diffY > 0) {
+                    onFlingBottom();
+                } else {
+                    onFlingTop();
+                }
+                result = true;
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return result;
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        if (this.mDetector.onTouchEvent(event)) {
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+    public void onFlingRight() {
+        Intent intentright = new Intent (Change_personal_info.this, Profile.class);
+        startActivity(intentright);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public boolean onDown(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent motionEvent) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent motionEvent) {
+
+    }
+
+
+
+    public void onFlingLeft() {
+
+    }
+
+
+
+    public void onFlingTop() {
+
+    }
+
+    public void onFlingBottom() {
+
+    }
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
+        return false;
+    }
+}
--- app/src/main/java/com/example/kollect_final_project/Profile.java
+++ app/src/main/java/com/example/kollect_final_project/Profile.java
@@ -6,15 +6,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Profile extends AppCompatActivity {
+    private TextView change_info_txt;
+    private ImageView change_info_arr;
+    private TextView completed_purchase_txt;
+    private ImageView completed_purchase_arr;
+    private TextView pre_txt;
+    private ImageView pre_arr;
+    private TextView get_pre_txt;
+    private ImageView get_pre_arr;
+    private TextView blacklist_txt;
+    private ImageView blacklist_arr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        change_info_txt = (TextView)findViewById(R.id.change_info_txt);
+        change_info_arr = (ImageView)findViewById(R.id.change_info_arr);
+        completed_purchase_txt = (TextView)findViewById(R.id.completed_purchase_txt);
+        completed_purchase_arr = (ImageView)findViewById(R.id.completed_purchase_arr);
+        pre_txt = (TextView)findViewById(R.id.pre_txt);
+        pre_arr = (ImageView)findViewById(R.id.pre_arr);
+        get_pre_txt = (TextView)findViewById(R.id.get_pre_txt);
+        get_pre_arr = (ImageView)findViewById(R.id.get_pre_arr);
+        blacklist_txt = (TextView)findViewById(R.id.blacklist_txt);
+        blacklist_arr = (ImageView)findViewById(R.id.blacklist_arr);
+
+        change_info_txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent change_info = new Intent(Profile.this, Change_personal_info.class);
+                startActivity(change_info);
+            }
+        });
+        change_info_arr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent change_info = new Intent(Profile.this, Change_personal_info.class);
+                startActivity(change_info);
+            }
+        });
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.profile);
