639ed269ad4afeaea3d8f0c3a5e9c980b8e68c90
lzivan <michael20001122@gmail.com>
2022-11-16 19:52:53 -0500

    Revert "Merge branch 'main' of https://github.com/Cecilixy/Kollect_Final_Project"
    This reverts commit 59f53c27dc7b8716741e8c7bf5aa078457e83262, reversing
    changes made to 47193ebfb8022993c7d7ee4d0fbda147a9d7a316.

--- app/src/main/java/com/example/kollect_final_project/UserProfileUpdate.java
+++ app/src/main/java/com/example/kollect_final_project/UserProfileUpdate.java
@@ -1,12 +1,9 @@
 package com.example.kollect_final_project;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GestureDetectorCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -17,11 +14,7 @@ import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class UserProfileUpdate extends AppCompatActivity implements         GestureDetector.OnGestureListener,
-        GestureDetector.OnDoubleTapListener {
-    private GestureDetectorCompat mDetector;
-    private static final int thresh = 100;
-    private static final int velocity = 100;
+public class UserProfileUpdate extends AppCompatActivity {
 
     TextInputLayout fullName, instalink, gender, password;
     TextView fullNameLabel, usernameLabel;
@@ -36,8 +29,6 @@ public class UserProfileUpdate extends AppCompatActivity implements         Gest
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile_update);
 
-        mDetector = new GestureDetectorCompat(this,this);
-
         reference = FirebaseDatabase.getInstance().getReference("Users");
 
         //Hooks
@@ -51,47 +42,6 @@ public class UserProfileUpdate extends AppCompatActivity implements         Gest
         //ShowAllData
         showAllUserData();
 
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        boolean result = false;
-        try {
-            float diffY = e2.getY() - e1.getY();
-            float diffX = e2.getX() - e1.getX();
-            if (Math.abs(diffX) > Math.abs(diffY)) {
-                if (Math.abs(diffX) > thresh && Math.abs(velocityX) > velocity) {
-                    if (diffX > 0) {
-                        onFlingRight();
-                    } else {
-                        onFlingLeft();
-                    }
-                    result = true;
-                }
-            }
-            else if (Math.abs(diffY) > thresh && Math.abs(velocityY) > velocity) {
-                if (diffY > 0) {
-                    onFlingBottom();
-                } else {
-                    onFlingTop();
-                }
-                result = true;
-            }
-        } catch (Exception exception) {
-            exception.printStackTrace();
-        }
-        return result;
-    }
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        if (this.mDetector.onTouchEvent(event)) {
-            return true;
-        }
-        return super.onTouchEvent(event);
-    }
-    public void onFlingRight() {
-        Intent intentright = new Intent (UserProfileUpdate.this, Profile.class);
-        startActivity(intentright);
     }
 
     private void showAllUserData() {
@@ -142,64 +92,5 @@ public class UserProfileUpdate extends AppCompatActivity implements         Gest
             return false;
         }
 
-    }
-
-
-
-
-
-    @Override
-    public boolean onDown(MotionEvent motionEvent) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent motionEvent) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent motionEvent) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent motionEvent, MotionEvent motionEvent1, float v, float v1) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent motionEvent) {
-
-    }
-
-
-
-    public void onFlingLeft() {
-
-    }
-
-
-
-    public void onFlingTop() {
-
-    }
-
-    public void onFlingBottom() {
-
-    }
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent motionEvent) {
-        return false;
-    }
-
-    @Override
-    public boolean onDoubleTap(MotionEvent motionEvent) {
-        return false;
-    }
-
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent motionEvent) {
-        return false;
     }
 }
