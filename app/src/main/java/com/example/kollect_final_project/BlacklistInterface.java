9329cbef8859c9d5739db902c29a42f702f82200
Tianyi Bao <tianyib831@gmail.com>
2022-11-17 03:38:55 -0500

    Blacklist cleanup

--- app/src/main/java/com/example/kollect_final_project/BlacklistInterface.java
+++ app/src/main/java/com/example/kollect_final_project/BlacklistInterface.java
@@ -3,12 +3,21 @@ package com.example.kollect_final_project;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 public class BlacklistInterface extends AppCompatActivity {
     private Button btnAdd, btnGetAll;
+
+    private ListView listView;
+    private ArrayList<Blacklist> blacklistModelArrayList;
+    private CustomAdapterBlacklist customAdapterBlacklist;
+    private MySQLiteOpenHelper databaseHelper;
 
 
     @Override
@@ -16,7 +25,27 @@ public class BlacklistInterface extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blacklist);
         btnAdd = (Button) findViewById(R.id.Add_Blacklist_btn);
-        btnGetAll = (Button) findViewById(R.id.getAllBlacklist);
+
+        listView = (ListView) findViewById(R.id.listview);
+
+        databaseHelper = new MySQLiteOpenHelper(this);
+
+        blacklistModelArrayList = databaseHelper.getAllBlacklist();
+
+        customAdapterBlacklist = new CustomAdapterBlacklist(this,blacklistModelArrayList);
+        listView.setAdapter(customAdapterBlacklist);
+
+        /*
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(BlacklistInterface.this, UpdateandDeleteUsers.class);
+                intent.putExtra("user", blacklistModelArrayList.get(position));
+                startActivity(intent);
+            }
+        });
+
+         */
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,12 +55,5 @@ public class BlacklistInterface extends AppCompatActivity {
             }
         });
 
-        btnGetAll.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(BlacklistInterface.this, getAllBlacklist.class);
-                startActivity(intent);
-            }
-        });
     }
 }
